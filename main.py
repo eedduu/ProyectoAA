0871e52bbc36a3e187612386532808b371c6e585
RubenGiC <rubengirela@gmail.com>
2021-06-14 21:08:55 +0200

    Graficas Cross-Validation

--- main.py
+++ main.py
@@ -116,75 +116,80 @@ X_train = pca.fit_transform(X_train, Y_train)
 X_train = np.append(X_train, columnas, axis=1)
 
 #%% Embedd 2D
-X_embedded = TSNE(n_components=2).fit_transform(X_train)
+# X_embedded = TSNE(n_components=2).fit_transform(X_train)
 
-#%% Visualizacion 2D
-plt.scatter(X_embedded[:,0],X_embedded[:,1], c=Y_train)
-plt.show()
+# #%% Visualizacion 2D
+# plt.scatter(X_embedded[:,0],X_embedded[:,1], c=Y_train)
+# plt.show()
 
 #%% Optimizacion de parametros
 
 #array de valores de parametros razonables para la obtencion de parametros
-print("comprobando parametros a usar: ")
-print("--------------------------------------------------------------------------------------------------")
-param_grid = {'penalty': ['l2'], 'loss':['hinge', 'squared_hinge'], 'dual': [False], 'random_state': [0]}
-modelo = GridSearchCV(svm.LinearSVC(), param_grid, scoring='roc_auc')
-modelo.fit(X_train, Y_train)
-print("--------------------------------------------------------------------------------------------------")
-print('Mejores parámetros del modelo LinearSVC: ', modelo.best_params_)
+# print("comprobando parametros a usar: ")
+# print("--------------------------------------------------------------------------------------------------")
+# param_grid = {'penalty': ['l2'], 'loss':['hinge', 'squared_hinge'], 'dual': [False], 'random_state': [0]}
+# modelo = GridSearchCV(svm.LinearSVC(), param_grid, scoring='roc_auc')
+# modelo.fit(X_train, Y_train)
+# print("--------------------------------------------------------------------------------------------------")
+# print('Mejores parámetros del modelo LinearSVC: ', modelo.best_params_)
 
 #%% Linear SVC
 
-#creamos el modelo
-clf = svm.LinearSVC( dual=False, random_state=0)
+#lista de resultados para comparar
+list_data = []
+nombres = ["LinearSVC", "MLP", "RandomForest"]
 
-#y entrenamos el modelo con la muestra dada
-results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
-print('AUC  en Cross-Validation con LinearSVC (sin regularizacion)', results['test_score'].mean())
+# #creamos el modelo
+# clf = svm.LinearSVC( dual=False, random_state=0)
+
+# #y entrenamos el modelo con la muestra dada
+# results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
+# print('AUC  en Cross-Validation con LinearSVC (sin regularizacion)', results['test_score'].mean())
 
 
 clf = svm.LinearSVC(C=0.00000000000000001, dual=False, random_state=0)
 
 results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
 print('AUC  en Cross-Validation con LinearSVC (con regularizacion)', results['test_score'].mean())
+list_data.append(results['test_score'].mean());
 
 #%% Optimizacion parametros NN
-print("--------------------------------------------------------------------------------------------------1")
-param_grid = {'hidden_layer_sizes': [[50, 50], [50, 60], [50, 70], [50, 80], [50, 90], [50, 100],
-                                [60,50], [60,60], [60,70], [60,80], [60,90], [60,100], 
-                                [70, 50], [70, 60], [70, 70], [70, 80], [70, 90], [70, 100], 
-                                [80,50], [80,60], [80,70], [80,80], [80,90], [80,100], 
-                                [90,50], [90,60], [90,70], [90,80], [90,90], [90,100],
-                                [100,50], [100,60], [100,70], [100,80], [100,90], [100,100], ] }
+# print("--------------------------------------------------------------------------------------------------1")
+# param_grid = {'hidden_layer_sizes': [[50, 50], [50, 60], [50, 70], [50, 80], [50, 90], [50, 100],
+#                                 [60,50], [60,60], [60,70], [60,80], [60,90], [60,100], 
+#                                 [70, 50], [70, 60], [70, 70], [70, 80], [70, 90], [70, 100], 
+#                                 [80,50], [80,60], [80,70], [80,80], [80,90], [80,100], 
+#                                 [90,50], [90,60], [90,70], [90,80], [90,90], [90,100],
+#                                 [100,50], [100,60], [100,70], [100,80], [100,90], [100,100], ] }
 
-modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
-modelo.fit(X_train, Y_train)
-print("--------------------------------------------------------------------------------------------------")
+# modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
+# modelo.fit(X_train, Y_train)
+# print("--------------------------------------------------------------------------------------------------")
 
-print('Mejores parámetros (neuronas por capa) del Perceptron de 3 capas: ', modelo.best_params_)
+# print('Mejores parámetros (neuronas por capa) del Perceptron de 3 capas: ', modelo.best_params_)
 
-#Ajustamos un poco más
-print("--------------------------------------------------------------------------------------------------")
-param_grid = {'hidden_layer_sizes': [[50, 50], [50,55], [55,50], [52,55], [55,52], [55,55]]}
-modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
-modelo.fit(X_train, Y_train)
-print("--------------------------------------------------------------------------------------------------")
-print('Mejores parámetros (neuronas por capa) del Perceptron de 3 capas: ', modelo.best_params_)
-print("--------------------------------------------------------------------------------------------------")
-param_grid= {'solver':['lbfgs', 'adam', 'sgd'], 'hidden_layer_sizes': [[52, 55]], 'activation':['logistic', 'tanh', 'relu'], 'learning_rate_init':[0.001, 0.01, 0.0001], 'learning_rate':['constant', 'invscaling', 'adaptative']}
-modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
-modelo.fit(X_train, Y_train)
-print("--------------------------------------------------------------------------------------------------")
+# #Ajustamos un poco más
+# print("--------------------------------------------------------------------------------------------------")
+# param_grid = {'hidden_layer_sizes': [[50, 50], [50,55], [55,50], [52,55], [55,52], [55,55]]}
+# modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
+# modelo.fit(X_train, Y_train)
+# print("--------------------------------------------------------------------------------------------------")
+# print('Mejores parámetros (neuronas por capa) del Perceptron de 3 capas: ', modelo.best_params_)
+# print("--------------------------------------------------------------------------------------------------")
+# param_grid= {'solver':['lbfgs', 'adam', 'sgd'], 'hidden_layer_sizes': [[52, 55]], 'activation':['logistic', 'tanh', 'relu'], 'learning_rate_init':[0.001, 0.01, 0.0001], 'learning_rate':['constant', 'invscaling', 'adaptative']}
+# modelo = GridSearchCV(MLPClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
+# modelo.fit(X_train, Y_train)
+# print("--------------------------------------------------------------------------------------------------")
 
-print('Mejores parámetros del Perceptron de 3 capas: ', modelo.best_params_)
+# print('Mejores parámetros del Perceptron de 3 capas: ', modelo.best_params_)
 
 
-#%% Multilayer perceptron CV
-clf = MLPClassifier(hidden_layer_sizes=[52, 55], activation='logistic', learning_rate='constant', learning_rate_init=0.01, solver='sgd'  )
-clf.fit(X_train, Y_train)
-results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
+# #%% Multilayer perceptron CV
+# clf = MLPClassifier(hidden_layer_sizes=[52, 55], activation='logistic', learning_rate='constant', learning_rate_init=0.01, solver='sgd'  )
+# clf.fit(X_train, Y_train)
+# results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
 
-print('Perceptrón 3 capas sin regularizacion en Cross-Validation AUC Score', results['test_score'].mean())
+# print('Perceptrón 3 capas sin regularizacion en Cross-Validation AUC Score', results['test_score'].mean())
 
 
 
@@ -193,28 +198,29 @@ clf.fit(X_train, Y_train)
 results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
 
 print('Perceptrón 3 capas con regularizacion en Cross-Validation AUC Score', results['test_score'].mean())
+list_data.append(results['test_score'].mean());
 
 #%% RandomForest parametros
 start_time = time()
-print("--------------------------------------------------------------------------------------------------")
-param_grid = {'n_estimators': [100,200,300,400, 500], 'criterion': ['gini', 'entropy'], 'n_jobs':[-1], 'random_state':[0], 'bootstrap':[True, False], 'oob_score':[True, False]}
-modelo = GridSearchCV(RandomForestClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
-modelo.fit(X_train, Y_train)
-print("--------------------------------------------------------------------------------------------------")
-elapsed_time = time() - start_time
-print("Calculo Elapsed time: %0.10f seconds" %elapsed_time)
+# print("--------------------------------------------------------------------------------------------------")
+# param_grid = {'n_estimators': [100,200,300,400, 500], 'criterion': ['gini', 'entropy'], 'n_jobs':[-1], 'random_state':[0], 'bootstrap':[True, False], 'oob_score':[True, False]}
+# modelo = GridSearchCV(RandomForestClassifier(), param_grid, scoring='roc_auc', n_jobs=-1)
+# modelo.fit(X_train, Y_train)
+# print("--------------------------------------------------------------------------------------------------")
+# elapsed_time = time() - start_time
+# print("Calculo Elapsed time: %0.10f seconds" %elapsed_time)
 
-print('Mejores parámetros del Random Forest: ', modelo.best_params_)
+# print('Mejores parámetros del Random Forest: ', modelo.best_params_)
 
-#%% RandomForest CV
+# #%% RandomForest CV
 
 
-start_time = time()
-clf = RandomForestClassifier(n_estimators=500, criterion='entropy', oob_score=True, n_jobs=-1, random_state=0)
-results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
-elapsed_time = time() - start_time
-print("Calculo Elapsed time: %0.10f seconds" %elapsed_time)
-print('Random Forest (sin regularizacion) en Cross-Validation AUC Score', results['test_score'].mean())
+# start_time = time()
+# clf = RandomForestClassifier(n_estimators=500, criterion='entropy', oob_score=True, n_jobs=-1, random_state=0)
+# results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_auc')
+# elapsed_time = time() - start_time
+# print("Calculo Elapsed time: %0.10f seconds" %elapsed_time)
+# print('Random Forest (sin regularizacion) en Cross-Validation AUC Score', results['test_score'].mean())
 
 
 start_time = time()
@@ -223,7 +229,13 @@ results = cross_validate(clf, X_train, Y_train, cv=5, n_jobs=-1, scoring='roc_au
 elapsed_time = time() - start_time
 print("Calculo Elapsed time: %0.10f seconds" %elapsed_time)
 print('Random Forest (con regularizacion) en Cross-Validation AUC Score', results['test_score'].mean())
+list_data.append(results['test_score'].mean());
 
+#%% Grafica de barras de Cross-Validation
+fig, ax = plt.subplots()
+ax.bar(nombres,list_data)
+fig.suptitle('Comparación Cross-Validation')
+fig.show()
 
 
 #%% Preparo datos test
@@ -244,6 +256,10 @@ X_test = np.append(X_test, columnas, axis=1)
 
 
 #%% Calculo Ein y Eout Random Forest
+
+list_ein=[]
+list_etest=[]
+
 clf = RandomForestClassifier(n_estimators=500, criterion='entropy', oob_score=True, max_leaf_nodes=4, n_jobs=-1, random_state=0)
 clf.fit(X_train, Y_train)
 print('AUC score RF train', roc_auc_score(Y_train, clf.predict(X_train)))
@@ -260,6 +276,9 @@ print('Defecto Accuracy score RF  train', accuracy_score(Y_train, clf.predict(X_
 print('Defecto AUC score RF test', roc_auc_score(Y_test, clf.predict(X_test)))
 print('Defecto Accuracy score RF test', accuracy_score(Y_test, clf.predict(X_test)))
 
+list_ein.append(roc_auc_score(Y_train, clf.predict(X_train)))
+list_etest.append(roc_auc_score(Y_test, clf.predict(X_test)))
+
 clf = RandomForestClassifier(n_estimators=500, criterion='entropy', oob_score=True, n_jobs=-1, random_state=0)
 clf.fit(X_train, Y_train)
 print('SR AUC score RF train', roc_auc_score(Y_train, clf.predict(X_train)))
@@ -267,6 +286,9 @@ print('SR Accuracy score RF  train', accuracy_score(Y_train, clf.predict(X_train
 
 print('SR AUC score RF test', roc_auc_score(Y_test, clf.predict(X_test)))
 print('SR Accuracy score RF test', accuracy_score(Y_test, clf.predict(X_test)))
+
+list_ein.append(roc_auc_score(Y_train, clf.predict(X_train)))
+list_etest.append(roc_auc_score(Y_test, clf.predict(X_test)))
 
 
 
@@ -280,7 +302,11 @@ print('Accuracy score Dummy  train', accuracy_score(Y_train, clf.predict(X_train
 print('AUC score poly Dummy test', roc_auc_score(Y_test, clf.predict(X_test)))
 print('Accuracy score Dummy test', accuracy_score(Y_test, clf.predict(X_test)))
 
-#%% Calculo Ein y Eout Random Forest
+list_ein.append(roc_auc_score(Y_train, clf.predict(X_train)))
+list_etest.append(roc_auc_score(Y_test, clf.predict(X_test)))
+
+
+#%% Calculo Ein y Eout MLP
 clf = MLPClassifier(hidden_layer_sizes=[52, 55], activation='logistic', learning_rate='constant', learning_rate_init=0.01, solver='sgd' )
 clf.fit(X_train, Y_train)
 print('AUC score RF train', roc_auc_score(Y_train, clf.predict(X_train)))
@@ -296,3 +322,22 @@ print('Accuracy score RF  train', accuracy_score(Y_train, clf.predict(X_train)))
 
 print('AUC score RF test', roc_auc_score(Y_test, clf.predict(X_test)))
 print('Accuracy score RF test', accuracy_score(Y_test, clf.predict(X_test)))
+
+#%% Grafica Ein y Etest
+nombres = ['Dummy', 'RandomForest Default','RandomForest Sin Regularización']
+x = np.arange(len(nombres))
+width = 0.35
+
+fig2, ax2 = plt.subplots()
+
+rect1 = ax.bar(x-width/2, list_ein, width, label='Ein')
+rect2 = ax.bar(x+width/2, list_etest, width, label='Etest')
+
+ax2.set_xticks(x)
+ax2.set_xtickslabels(nombres)
+ax2.lengend()
+ax2.bar_label(rect1, padding=3)
+ax2.bar_label(rect2, padding=3)
+fig2.suptitle('Comparación Ein y Etest')
+fig2.tight_layout()
+fig2.show()
